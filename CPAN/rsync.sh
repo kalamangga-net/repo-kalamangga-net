e47134d9ff3f1c8ad5aeb4ceef4e02c8f7c89ff2
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2015-07-22 09:28:11 +0700

    CPAN rsync script

---
+++ CPAN/rsync.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+REPO_DIR="/local/mirror"
+REPO_APP="CPAN"
+REPO_URL="rsync://kambing.ui.ac.id/CPAN/"
+REPO_TMP="$REPO_DIR/tmp"
+
+mkdir -p $REPO_TMP
+PIDFILE="$REPO_DIR/tmp/$REPO_APP.pid"
+
+if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
+                           grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
+  echo "Already running."
+  exit 99
+fi
+
+rsync -rtlzv --delete $REPO_URL $REPO_DIR/$REPO_APP/ > $REPO_TMP/$REPO_APP.log &
+
+echo $! > "${PIDFILE}"
+chmod 644 "${PIDFILE}"
