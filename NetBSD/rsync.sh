2f7053d2b6de0ee18366e61575b03d017b2feeab
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2015-07-22 09:34:02 +0700

    NetBSD rsync script

---
+++ NetBSD/rsync.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+REPO_DIR="/local/mirror"
+REPO_APP="NetBSD"
+REPO_URL="rsync://rsync.tw.NetBSD.org/NetBSD/"
+REPO_TMP="$REPO_DIR/tmp"
+
+mkdir -p $REPO_TMP
+PIDFILE="$REPO_TMP/$REPO_APP.pid"
+
+if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
+                           grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
+  echo "Already running."
+  exit 99
+fi
+
+rsync -rtlzv --delete $REPO_URL $REPO_DIR/$REPO_APP/ > $REPO_TMP/$REPO_APP.log &
+
+echo $! > "${PIDFILE}"
+chmod 644 "${PIDFILE}"
