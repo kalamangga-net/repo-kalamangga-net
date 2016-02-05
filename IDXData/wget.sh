7b99bbc84e276d06496a8b05b7a609c5ac9d06b0
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2016-02-05 22:45:27 +0700

    add komen

--- IDXData/wget.sh
+++ IDXData/wget.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
+# Konfigurasi
 REPO_DIR="/local/mirror"
 REPO_APP="IDXData"
 REPO_URL="http://www.idxdata.co.id"
 REPO_TMP="$REPO_DIR/tmp"
 
+# Pid
 mkdir -p $REPO_TMP
 PIDFILE="$REPO_TMP/$REPO_APP.pid"
 
+# Cek pid, hentikan proses jika ada
 if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
                            grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
   echo "Already running."
   exit 99
 fi
 
+# Sinkronisasi, simpan dalam log
 mkdir -p $REPO_DIR/$REPO_APP
 cd $REPO_DIR/$REPO_APP
 wget -m -nH -t 1 -w 1 $REPO_URL > $REPO_TMP/$REPO_APP.log 2>&1 &
 
+# Bersihkan index.html yang terbentuk
 for i in `find $REPO_DIR/$REPO_APP/ -name "index.html"`
 do
  rm $i
 done
 
+# Isi pid
 echo $! > "${PIDFILE}"
 chmod 644 "${PIDFILE}"
--- NetBSD/rsync.sh
+++ NetBSD/rsync.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+# Konfigurasi
 REPO_DIR="/local/mirror"
 REPO_APP="NetBSD"
 REPO_URL="rsync://rsync.tw.NetBSD.org/NetBSD/"
 REPO_TMP="$REPO_DIR/tmp"
 
+# Pid
 mkdir -p $REPO_TMP
 PIDFILE="$REPO_TMP/$REPO_APP.pid"
 
+# Cek pid, hentikan proses jika ada
 if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
                            grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
   echo "Already running."
   exit 99
 fi
 
+# Sinkronisasi, simpan dalam log
 rsync -rtlzv --delete $REPO_URL $REPO_DIR/$REPO_APP/ > $REPO_TMP/$REPO_APP.log &
 
+# Isi pid
 echo $! > "${PIDFILE}"
 chmod 644 "${PIDFILE}"
